7cea1657a0468b26eb917dcbb0a8bf256222c819
LeonEidelman <leoneidelman09@gmail.com>
2025-11-27 10:43:05 +0200

    header scroll and styles

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -62,7 +62,7 @@ const AboutPage: React.FC<AboutPageProps> = (
                 Patient Guides
               </button>
               <button
-                className="btn btn-tertiary"
+                className="btn btn-tertiary btn-tertiary-mobile"
                 onClick={() => navigate('/innovation')}
               >
                 Surgical Innovation
